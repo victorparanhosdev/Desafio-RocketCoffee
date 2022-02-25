8cefc0e38f17cd8f80ff2a7a340681e9e07e07dd
Victor Paranhos <victor_paranhos@hotmail.com>
2022-02-25 12:17:02 -0300

    up 9

--- script/funcoes.js
+++ script/funcoes.js
@@ -1,2 +1,3 @@
-document.querySelector(".menu-mobile").addEventListener("click", () => {
-    document.querySelector(".menu-meio").classList.toggle("on")})
+document.querySelector(".botao-menumobileopen").addEventListener("click", () => {
+    document.querySelector(".menu-meio").classList.toggle("on")
+document.querySelector(".botao-menumobileopen").setAttribute('src', "/assets/menu-buguer-close.svg")})
