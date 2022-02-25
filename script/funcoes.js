5cc7f490d34c6c0de6d09ca986223219287f7d9c
Victor Paranhos <victor_paranhos@hotmail.com>
2022-02-25 14:15:15 -0300

    Update 10

--- script/funcoes.js
+++ script/funcoes.js
@@ -1,3 +1,26 @@
-document.querySelector(".botao-menumobileopen").addEventListener("click", () => {
-    document.querySelector(".menu-meio").classList.toggle("on")
-document.querySelector(".botao-menumobileopen").setAttribute('src', "/assets/menu-buguer-close.svg")})
+const botaoMobile = document.querySelector(".botao-menumobileopen");
+const menuMeio = document.querySelector(".menu-meio");
+let show = true;
+
+
+botaoMobile.addEventListener("click", () => {
+
+    if(show) {
+        
+        botaoMobile.setAttribute('src', "./assets/menu-buguer-close.svg")
+        menuMeio.classList.add("on")
+        //document.body.style.background = "red"
+        show = false
+    } else {
+        botaoMobile.setAttribute('src', "./assets/menu-buguer-open.svg")
+        menuMeio.classList.remove("on")
+        //document.body.style.background = "green"
+        show = true
+    }
+
+
+
+})
+
+
+
