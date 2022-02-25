70f753a5c330c6b7ba4fce51cd410a91128e8452
Victor Paranhos <victor_paranhos@hotmail.com>
2022-02-25 20:28:57 -0300

    Finalizado 3

--- script/funcoes.js
+++ script/funcoes.js
@@ -1,5 +1,6 @@
 const botaoMobile = document.querySelector(".botao-menumobileopen");
 const menuMobile = document.querySelector(".menu-mobile");
+const conteudo = document.querySelector(".conteudo-mobile")
 
 let show = true;
 
@@ -10,16 +11,16 @@ botaoMobile.addEventListener("click", () => {
         
         botaoMobile.setAttribute('src', "./assets/menu-buguer-close.svg")
         menuMobile.classList.add("on")
+        conteudo.classList.add("on")
         document.body.style.overflow = "hidden"
         show = false
     } else {
         botaoMobile.setAttribute('src', "./assets/menu-buguer-open.svg")
         menuMobile.classList.remove("on")
+        conteudo.classList.remove("on")
         document.body.style.overflow = "initial"
         show = true
     }
-
-
 
 })
 
